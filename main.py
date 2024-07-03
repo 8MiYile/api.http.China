822d57c7a4e1c4cbafafa5170ad3faa4ca056122
8Mi_Yile <admin@8mi.tech>
2024-07-03 17:11:58 +0800

    change to v2

---
+++ main.py
@@ -0,0 +1,39 @@
+#import httpx
+#from flask_caching import Cache
+from flask import Flask, abort, redirect
+import random
+
+app = Flask(__name__)
+@app.route('/<path:type>/<path:http_code>')
+def index(type,http_code):
+    if type == '' or http_code == '':
+        abort(404)
+    
+    noun_cat=['cat']
+    noun_suffix = ['dog','pizza','garden','fish']
+    noun_main = ['ducks','cats','goats',]
+    nouns = noun_suffix + noun_main + noun_cat
+
+    if type == 'random':
+        url_main = "api.http.中国/" + nouns[random.randint(0,len(nouns)-1)]
+    if search_string(noun_suffix,type) or search_string(noun_cat,type):
+        url_main = "http." + http_code
+    if search_string(noun_main,type):
+        url_main = "http" + http_code + ".com"
+    if search_string(noun_cat,type):
+        redirect_response = redirect("https://"+url_main+"/"+http_code)
+    if search_string(nouns,type):
+        redirect_response = redirect("https://"+url_main+"/"+http_code.jpg)
+        #redirect_response.headers['Cache-Control'] = 'max-age=0, s-maxage=300'
+    return redirect_response
+
+def search_string(list,target_string):
+    if target_string in list:
+        #index = list.index(target_string)
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    app.run()
